2b892263d986b908b84282fa926663b622f5aba6
Michał Urbański <michalurb8@gmail.com>
2022-11-30 17:50:54 +0100

    koniec warsztatow 30.11

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import time
-from spade import agent,quit_spade
+from spade import agent, quit_spade
+from aioxmpp import PresenceShow
 
 lista = ['frodo', 'gandalf', 'aragorn', 'pippin']
+lista = ['frodo', 'gandalf']
 class MyAgent(agent.Agent):
-    async def setup(self):
+    async def setup(self) -> None:
         print("Agent {0} melduje sie".format(str(self.jid)))
+        self.presence.set_available(show = PresenceShow.CHAT)
 
 agents = []
 for name in lista:
     myAgent=MyAgent(name+"@hookipa.net", "polskagora")
     agents.append(myAgent)
     future=myAgent.start()
+    future.result()
+
 agents[0].web.start(hostname = "127.0.0.1", port = '10000')
 agents[1].web.start(hostname = "127.0.0.1", port = '10001')
-agents[2].web.start(hostname = "127.0.0.1", port = '10002')
-agents[3].web.start(hostname = "127.0.0.1", port = '10003')
-time.sleep(60)
+agents[0].presence.subscribe(str(agents[1].jid))
+agents[1].presence.subscribe(str(agents[0].jid))
+time.sleep(120)
 for a in agents:
     a.stop()
 quit_spade()
