eaa6218564513d4d6fc1eca4c60c2dacc6e06a7c
Mateusz Kordowski <mateuszkd67@gmail.com>
2022-12-20 18:46:04 +0100

    remove unused var

--- defaults.py
+++ defaults.py
@@ -10,7 +10,6 @@ PROJECT_VARS = {
     'ENV_TICKS': 1.,  # per seconds, env ticks in one update, the more the faster simulation is
     'ENV_SIM_SPEED': 1., # per second, the speed at which simulated clock is ticking
     'ENV_SLEEP_TYPE': 'LOOP', # 'SLEEP' or 'LOOP'
-    'SURFACE_SQUARE_LENGTH': 100 # in meters
 }
 
 logging.basicConfig(
