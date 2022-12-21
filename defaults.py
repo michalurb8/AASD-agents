78baff34ee7534fa8cef8ee871505bc3556a1463
Mateusz Kordowski <mateuszkd67@gmail.com>
2022-12-21 21:44:06 +0100

    refactor of env, cyclic behav; added local env

--- defaults.py
+++ defaults.py
@@ -5,11 +5,13 @@ PROJECT_VARS = {
     'SERVER': 'localhost',
     'ENV_PASSW': 'polskagora',
     'ENV_AGENT_NAME': 'environment',
-    'ENV_LOGGER_NAME': 'Environment',
+    'GLOB_ENV_LOGGER_NAME': 'Global Environment',
+    'LOCAL_ENV_LOGGER_NAME': 'Local Environment',
     'LOG_LEVEL': logging.INFO,
     'ENV_TICKS': 1.,  # per seconds, env ticks in one update, the more the faster simulation is
     'ENV_SIM_SPEED': 1., # per second, the speed at which simulated clock is ticking
     'ENV_SLEEP_TYPE': 'LOOP', # 'SLEEP' or 'LOOP'
+    'LOG_DIR': 'logs',
 }
 
 logging.basicConfig(
--- environment.py
+++ environment.py
@@ -7,6 +7,7 @@ from spade_fix.behaviour import CyclicBehaviour
 from defaults import PROJECT_VARS
 from surface import Surface
 import numpy as np
+from abc import abstractmethod
 
 ENV_TIME_SIM_SECONDS = 'simulated_runtime'
 ENV_TIME_RUNTIME_SECONDS = 'runtime'
@@ -31,96 +32,144 @@ class FPS():
             self.lastFPS = self.frameCounter / calcSecond
             self.frameCounter = 0
             self.lastT = current
-            
-class EnvironmentAgent(Agent):
-    class TimeBehav(CyclicBehaviour):
-        def __init__(self):
-            super().__init__()
-            self._firstRun = False
+
+class CyclicLogger():
+    def __init__(self, logger, loggerPrefix: str, timeout: int=1e+9) -> None:
+        self.logger = logger
+        self.loggerPrefix = loggerPrefix
+        self.timeout = timeout
+        self.storedData = {}
+        self.lastLogTime = time.time_ns()
+
+    def add(self, **kwargs) -> None:
+        self.storedData = self.storedData | kwargs
+
+    def tryFlush(self) -> None:
+        if(time.time_ns() - self.lastLogTime >= 1e+9):
+            s = f"{self.loggerPrefix} "
+            for k, v in self.storedData.items():
+                s += f"-{k}: {v}"
+            self.logger.info(s)
             self.lastLogTime = time.time_ns()
+            self.storedData = {}
 
-            self.behavTemplate = Template()
-            self.behavTemplate.thread = "EnvBehaviour"
-            self.behavTemplate.metadata = {"performative": "query"}
+class TimeBehaviour(CyclicBehaviour):
+    def __init__(self, logger, loggerPrefix: str, sleepType: str, envTicks: float, envSimSpeed: float):
+        super().__init__()
+        self._firstRun = False
+        self.cyclicLogger = CyclicLogger(logger=logger, loggerPrefix=loggerPrefix)
+        self.logger = logger
+        self.loggerPrefix = loggerPrefix
+        self.sleepType = sleepType
+        self.envTicks = envTicks
+        self.envSimSpeed = envSimSpeed
 
-        async def on_start(self) -> None:
-            self.logger = logging.getLogger(PROJECT_VARS['ENV_LOGGER_NAME'])
-            self.logger.info("Starting behaviour . . .")
+    async def on_start(self) -> None:
+        self.logger.info(f"Starting cyclic behaviour {self.loggerPrefix}. . .")
 
-            if(PROJECT_VARS['ENV_SLEEP_TYPE'] == 'SLEEP'):
-                self.sleepTypeF = EnvironmentAgent.TimeBehav.sleep_f
-            elif(PROJECT_VARS['ENV_SLEEP_TYPE'] == 'LOOP'):
-                self.sleepTypeF = EnvironmentAgent.TimeBehav.loop_f
-            else:
-                raise Exception(f"Unknown sleep type: {PROJECT_VARS['ENV_SLEEP_TYPE']}")
+        if(self.sleepType.upper() == 'SLEEP'):
+            self.sleepTypeF = TimeBehaviour.sleep_f
+        elif(self.sleepType.upper() == 'LOOP'):
+            self.sleepTypeF = TimeBehaviour.loop_f
+        else:
+            raise Exception(f"Unknown sleep type: {self.sleepType}")
 
-            self.stepCounter = 0
-            self.fps = FPS()
-            self.startTimeRuntime = time.time_ns()
-            self.currentTime = None
+        self.stepCounter = 0
+        self.fps = FPS()
+        self.startTimeRuntime = time.time_ns()
+        self.currentTime = None
 
-            self._initKnowledgeItems()
+        self._initKnowledgeItems()
 
-        def sleep_f(next_update_time):
-            diff = next_update_time - time.time_ns()
-            if(diff <= 0.): # better than max(diff, 0.)
-                time.sleep(diff)
+    def sleep_f(next_update_time):
+        diff = next_update_time - time.time_ns()
+        if(diff <= 0.): # better than max(diff, 0.)
+            time.sleep(diff)
 
-        def loop_f(next_update_time):
-            while True:
-                if time.time_ns() >= next_update_time:
-                    break
+    def loop_f(next_update_time):
+        while True:
+            if time.time_ns() >= next_update_time:
+                break
+    
+    def _customAwait(self): 
+        nanosecInSec = 1e+9
+        # scale based on one second
+        scale = 1. / self.envTicks
+        next_update_time = self.currentTime + nanosecInSec * scale
+        #next_second = float(math.ceil(t / nanosecInSec))
+        #next_update_time = next_second - nanosecInSec * scale
+        self.sleepTypeF(next_update_time)
+
+    def _setKnowledgeItems(self):
+        runtime = self._calcRuntime()
+        simtime = self._calcSimulatedTime()
+        fps = self.fps.get()
+        self.set('fps', fps)
+        self.set('stepCounter', self.stepCounter)
+        self.set(ENV_TIME_RUNTIME_SECONDS, runtime)
+        self.set(ENV_TIME_SIM_SECONDS, simtime)
+
+        self.cyclicLogger.add(
+            runtime=runtime, 
+            simulatedSec=simtime, 
+            step=self.stepCounter,
+            fps=self.fps.get(),
+        )
+
+    def _initKnowledgeItems(self):
+        self.set('fps', 0.)
+        self.set('stepCounter', self.stepCounter)
+        self.set(ENV_TIME_RUNTIME_SECONDS, 0.)
+        self.set(ENV_TIME_SIM_SECONDS, 0.)
+
+    def _calcRuntime(self) -> float:
+        return (time.time_ns() - self.startTimeRuntime) * 1e-9
+
+    def _calcSimulatedTime(self) -> float:
+        scale = self.envSimSpeed / self.envTicks
+        return (time.time_ns() - self.startTimeRuntime) * 1e-9 * scale
+
+    def isSetup(self) -> bool:
+        return self._firstRun
+
+    def startRun(self) -> None:
+        self.currentTime = time.time_ns()
+        self.stepCounter += 1
+        self.fps()
+
+    @abstractmethod
+    def middleRun(self) -> None:
+        pass
+
+    def endRun(self) -> None:
+        self._setKnowledgeItems()
+        self._firstRun = True
+        self._customAwait()    
+
+    async def run(self):
+        self.startRun()
+
+        self.middleRun()
         
-        def _customAwait(self): 
-            nanosecInSec = 1e+9
-            # scale based on one second
-            scale = 1. / PROJECT_VARS['ENV_TICKS']
-            next_update_time = self.currentTime + nanosecInSec * scale
-            #next_second = float(math.ceil(t / nanosecInSec))
-            #next_update_time = next_second - nanosecInSec * scale
-            self.sleepTypeF(next_update_time)
+        self.endRun()
 
-        def _setKnowledgeItems(self):
-            runtime = self._calcRuntime()
-            simtime = self._calcSimulatedTime()
-            fps = self.fps.get()
-            self.set('fps', fps)
-            self.set('stepCounter', self.stepCounter)
-            self.set(ENV_TIME_RUNTIME_SECONDS, runtime)
-            self.set(ENV_TIME_SIM_SECONDS, simtime)
+class GlobalEnvTimeBehaviour(TimeBehaviour):
+    def middleRun(self) -> None:
+        pass
 
-            if(time.time_ns() - self.lastLogTime >= 1e+9):
-                self.logger.info(f"Environment -step: {self.stepCounter} -fps: {self.fps.get()}"
-                    + f" -simulatedSec: {simtime} -runtime {runtime}")
-                self.lastLogTime = time.time_ns()
-
-        def _initKnowledgeItems(self):
-            self.set('fps', 0.)
-            self.set('stepCounter', self.stepCounter)
-            self.set(ENV_TIME_RUNTIME_SECONDS, 0.)
-            self.set(ENV_TIME_SIM_SECONDS, 0.)
-
-        def _calcRuntime(self) -> float:
-            return (time.time_ns() - self.startTimeRuntime) * 1e-9
-
-        def _calcSimulatedTime(self) -> float:
-            scale = PROJECT_VARS['ENV_SIM_SPEED'] / PROJECT_VARS['ENV_TICKS']
-            return (time.time_ns() - self.startTimeRuntime) * 1e-9 * scale
-
-        async def run(self):
-            self.currentTime = time.time_ns()
-            self.stepCounter += 1
-            self.fps()
-
-            self._setKnowledgeItems()
-            self._firstRun = True
-            self._customAwait()        
-
+class GlobalEnvironmentAgent(Agent):
     async def setup(self):
-        self.logger = logging.getLogger(PROJECT_VARS['ENV_LOGGER_NAME'])
+        self.logger = logging.getLogger(PROJECT_VARS['GLOB_ENV_LOGGER_NAME'])
+        self.logger.addHandler(logging.FileHandler(PROJECT_VARS['LOG_DIR'] + 'globalEnv.log'))
 
         self.logger.info("Agent starting . . .")
-        self.timebehav = self.TimeBehav()
+        self.timebehav = GlobalEnvTimeBehaviour(
+            logger=self.logger,
+            loggerPrefix="Global Environment",
+            envSimSpeed=PROJECT_VARS['ENV_SIM_SPEED'],
+            envTicks=PROJECT_VARS['ENV_TICKS'],
+            sleepType=PROJECT_VARS['ENV_SLEEP_TYPE'],
+        )
         self.add_behaviour(self.timebehav)
 
         self.surface = Surface()
@@ -133,7 +182,7 @@ class EnvironmentAgent(Agent):
             while envAgent.isSetup():
                 continue
         """
-        return not self.timebehav._firstRun
+        return not self.timebehav.isSetup()
 
     async def addAgent(self, agent, typee:str, id, startPos=None) -> bool:
         if(typee == 'mobile'):
@@ -152,8 +201,58 @@ class EnvironmentAgent(Agent):
     async def setAgentPosition(self, id, position: np.ndarray[np.float32]):
         return self.surface.setPosition(id=id, position=position)
 
+    async def findMobileAgents(self, position: np.ndarray[np.float32], radius: float) -> list[(id, Agent)]:
+        return self.surface.findMobileAgents(position=position, radius=radius)
+
+    async def findStaticAgents(self, position: np.ndarray[np.float32], radius: float) -> list[(id, Agent)]:
+        return self.surface.findStaticAgents(position=position, radius=radius)
+
+    async def findAgents(self, position: np.ndarray[np.float32], radius: float) -> list[(id, Agent)]:
+        return self.surface.findAgents(position=position, radius=radius)
+
+class LocalEnvironment(Agent):
+        
+    async def setup(self) -> None:
+        self.logger = logging.getLogger(PROJECT_VARS['LOCAL_ENV_LOGGER_NAME'] + f'_{self.name}')
+        self.logger.addHandler(logging.FileHandler(PROJECT_VARS['LOG_DIR'] + f'localEnv.{self.name}.log'))
+
+        self.logger.info(f"Agent {self.name} starting . . .")
+        self.timebehav = GlobalEnvTimeBehaviour(
+            logger=self.logger,
+            loggerPrefix=f"Local Environment of agent {self.name}",
+            envSimSpeed=PROJECT_VARS['ENV_SIM_SPEED'],
+            envTicks=PROJECT_VARS['ENV_TICKS'],
+            sleepType=PROJECT_VARS['ENV_SLEEP_TYPE'],
+        )
+        self.add_behaviour(self.timebehav)
+
+    def isSetup(self):
+        """
+            Check if the setup of the environment behaviour is done.
+            Use this as:
+            
+            while envAgent.isSetup():
+                continue
+        """
+        return not self.timebehav.isSetup()
+
+    async def getPosition(self) -> np.ndarray[np.float32]:
+        pass
+
+    async def getTemperature(self) -> float:
+        pass
+
+    async def getPulse(self) -> float:
+        pass
+
+    async def getBloodPressure(self) -> float:
+        pass
+
+    async def getAcceleration(self) -> np.ndarray[np.float32]:
+        pass
+
 if __name__ == "__main__":
-    envAgent = EnvironmentAgent(
+    envAgent = GlobalEnvironmentAgent(
         PROJECT_VARS['ENV_AGENT_NAME'] + '@' + PROJECT_VARS['SERVER'], 
         PROJECT_VARS['ENV_PASSW']
     )
